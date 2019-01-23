8e32dd246e1cf350f79419c50bdb0afd3a4d29ee
Boadzie Daniel <boadziedaniel43@gmail.com>
2019-01-23 12:26:01 +0000

    Modified the index view to add a search form

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,39 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        ituner
-      </h1>
-      <h2 class="subtitle">
-        Awesome itunes search app 
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    <h1>Search iTunes</h1>
+    <br>
+    <v-layout>
+      <v-flex md4 offset-md4>
+        <v-form @submit.prevent="submit" ref="search" v-model="valid" lazy-validation>
+          <v-text-field v-model="search" label="search" required autofocus></v-text-field>
+        </v-form>
+      </v-flex>
+    </v-layout>
+    <!-- <form @submit.prevent="submit">
+      <input placeholder="Enter Artist Name" v-model="search" ref="search" autofocus>
+    </form>-->
+  </div>
 </template>
-
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
+  data() {
+    return {
+      search: ""
+    };
+  },
+  methods: {
+    submit(event) {
+      this.$router.push(`results/${this.search}`);
+    }
   }
-}
+};
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+<style scoped>
+* {
   text-align: center;
 }
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+h1 {
+  padding: 20px;
 }
 </style>
-
